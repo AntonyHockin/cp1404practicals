a6bb7533d0163acd1c2d42731c0cdfc2de31d981
AntonyHockin <tonyhockin22@hotmail.com>
2021-08-28 14:34:06 +1000

    Add quick picks program

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,1 +1,17 @@
+import random
 
+MINIMUM_NUMBER = 1
+MAXIMUM_NUMBER = 45
+
+number_of_picks = int(input("How many quick picks? "))
+for i in range(0, number_of_picks):
+    quick_picks = []
+    for j in range(0, 6):
+        number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+        while number in quick_picks:
+            number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+        quick_picks.append(number)
+    quick_picks.sort()
+    for j in range(0, len(quick_picks)):
+        print(f"{quick_picks[j]:2}", end=" ")
+    print()
